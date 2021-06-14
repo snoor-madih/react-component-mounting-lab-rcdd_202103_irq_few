fe64c09686c09d35e2794cb6644748aa726e4f50
snoor_madih <snoor.madih@gmail.com>
2021-06-14 09:04:52 +0000

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -7,34 +7,59 @@ class Timer extends Component {
   };
 
   // add your code here
+shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
+  componentDidUpdate() {
+    this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
+    this.timer.current.style.width = 240+this.state.time*5/1000+"px"
+    this.timer.current.style.height = 150+this.state.time*5/1000+"px"
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, this.props.updateInterval*1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
   render() {
-    const { time, color } = this.state;
+    const { time, color, className, logText } = this.state
     return (
-      <section className="Timer" style={{ background: color }}>
-        <h1>{time}</h1>
-        <button onClick={this.stopClock}>Stop</button>
-        <aside className="mountText">Mounted</aside>
-        <small onClick={this.handleClose}>X</small>
+      <section className="Timer" style={{background: color}} ref={this.timer}>
+
+        <h1>{ time }</h1>
+        <button onClick={ this.stopClock }>Stop</button>
+        <aside className="logText">{ logText }</aside>
+        <small onClick={ this.handleClose }>X</small>
+
       </section>
     );
   }
 
-  //clock functions
+ //clock functions
   clockTick = () => {
     this.setState(prevState => ({
-      time: prevState.time + 1
-    }));
-  };
+      time: prevState.time + this.props.updateInterval
+    }))
+  }
 
   stopClock = () => {
-    clearInterval(this.interval);
-  };
+    clearInterval(this.interval)
+    this.setState({className: "hidden"})
+  }
 
   // for the 'x' button,
   handleClose = () => {
-    this.props.removeTimer(this.props.id);
-  };
+    this.props.removeTimer(this.props.id)
+  }
+
+
 }
 
 export default Timer;
