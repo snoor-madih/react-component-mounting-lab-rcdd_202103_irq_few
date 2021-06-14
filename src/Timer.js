5e02c5314d80d75c0b7057ce8dcb5a0a5ecc2cd6
snoor_madih <snoor.madih@gmail.com>
2021-06-14 09:01:52 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -42,7 +42,7 @@ shouldComponentUpdate(nextProps, nextState) {
     );
   }
 
-
+ //clock functions
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + this.props.updateInterval
@@ -63,33 +63,3 @@ shouldComponentUpdate(nextProps, nextState) {
 }
 
 export default Timer;
-//   render() {
-//     const { time, color } = this.state;
-//     return (
-//       <section className="Timer" style={{ background: color }}>
-//         <h1>{time}</h1>
-//         <button onClick={this.stopClock}>Stop</button>
-//         <aside className="mountText">Mounted</aside>
-//         <small onClick={this.handleClose}>X</small>
-//       </section>
-//     );
-//   }
-
-//   //clock functions
-//   clockTick = () => {
-//     this.setState(prevState => ({
-//       time: prevState.time + 1
-//     }));
-//   };
-
-//   stopClock = () => {
-//     clearInterval(this.interval);
-//   };
-
-//   // for the 'x' button,
-//   handleClose = () => {
-//     this.props.removeTimer(this.props.id);
-//   };
-// }
-
-// export default Timer;
